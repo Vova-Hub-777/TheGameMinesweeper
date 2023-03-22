4b218bc1b4758dc6db9999ce27e9cb3c55be7ff9
Vova-Hub-777 <vovavitkovskiy47@gmail.com>
2023-03-22 12:45:36 +0200

    Оновлення функції __init__ та initialize, результатом чого є інтерфейс програми

--- gui_Intro.py
+++ gui_Intro.py
@@ -7,12 +7,20 @@ import sys
 
 class Window(QWidget):
     def __init__(self):
+        print("intro")
+        super().__init__()
+        self.count = 0
         self.initialize()
 
     def initialize(self):
         self.setGeometry(700, 300, 500, 400)
         layout = QVBoxLayout()
         self.setLayout(layout)
+
+        self.label = QLabel("Minesweeper")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setFont(QtGui.QFont('Hack', 15))
+        layout.addWidget(self.label)
 
 
 app = QApplication(sys.argv)
