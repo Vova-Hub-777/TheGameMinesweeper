c6c44bfca86ba8e790a1725bf7041c23836c189b
vitaliklevchenko <vitalik72004@gmail.com>
2023-03-22 17:03:15 +0100

    Добавлена функцію виходу з ігри.

--- MineSweeper/gui.py
+++ MineSweeper/gui.py
@@ -1,5 +1,7 @@
-import pygame
+import sys, pygame
 from MineSweeper.settings import *
+from pygame.locals import *
+from MineSweeper.gameLogic import GameLogic
 
 class GUI():
     def __init__(self, level): # reset
@@ -12,7 +14,16 @@ class GUI():
         self.screen = pygame.display.set_mode(SCREEN_SIZE)  # set display size
         pygame.display.set_caption('Minesweeper')  # set program name
         gameLevel=self.getLevel(level) # Receive level in tuple format.
+        arr = GameLogic.createMap(self, gameLevel) # create and save the map
+        OPENED = [[False for column in range(len(arr[0]))] for row in range(len(arr))] # Check if the column is open
+        CHECKED = [[False for column in range(len(arr[0]))] for row in range(len(arr))] # Check if flag is checked
+        self.draw_Cells(arr)  # Draw cells
 
+        while True:  # main game loop (call the methods needed for the game)
+            for event in pygame.event.get(): # Area that receives events in the window. For example, quit key, keyboard key, mouse click, etc.
+                if event.type == QUIT: # Keep the program running until you press the X key at the top (required)
+                    pygame.quit()
+                    sys.exit()
     def getLevel(self, level): # Get the level (need to fix it later)
         if level=='Beginner':
             return BEGINNER
@@ -28,3 +39,15 @@ class GUI():
             return SCREEN_SIZE_INTERMEDIATE
         elif level=='Expert':
             return SCREEN_SIZE_ADVANCED
+
+
+    def draw_Cells(self, arr):
+        COLUMN_COUNT = len(arr[0])
+        ROW_COUNT = len(arr)
+
+        for column_index in range(COLUMN_COUNT):
+            for row_index in range(ROW_COUNT):
+                rect = (CELL_SIZE * column_index, CELL_SIZE * row_index, CELL_SIZE, CELL_SIZE)
+                pygame.draw.rect(self.screen, GRAY, rect)
+                pygame.draw.rect(self.screen, BLACK, rect, 1)
+
