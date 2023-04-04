d4c545250db87c5959e91b7a5fc4415b12edfd37
Vova-Hub-777 <vovavitkovskiy47@gmail.com>
2023-04-04 21:25:46 +0300

    Модернізація тестів та оновлення .gitignore.

--- test_all.py
+++ test_all.py
@@ -1,5 +1,6 @@
 import pytest
 from MineSweeper.settings import *
+from MineSweeper.gameLogic import GameLogic
 
 
 def getLevel(level):  # Get the level (need to fix it later)
@@ -28,3 +29,24 @@ def test_getLevel(level, expected):
                          [("Beginner", (250, 250)), ("Intermediate", (500, 500)), ("Expert", (1000, 500))])
 def test_getScreenSize(level, expected):
     assert getScreenSize(level) == expected
+
+
+def test_createMap_beginner():
+    game_logic = GameLogic()
+    beginner_map = game_logic.createMap((5, 5, 5))
+    assert len(beginner_map) == 5
+    assert len(beginner_map[0]) == 5
+
+
+def test_createMap_intermediate():
+    game_logic = GameLogic()
+    intermediate_map = game_logic.createMap((10, 10, 10))
+    assert len(intermediate_map) == 10
+    assert len(intermediate_map[0]) == 10
+
+
+def test_createMap_advanced():
+    game_logic = GameLogic()
+    advanced_map = game_logic.createMap((20, 10, 20))
+    assert len(advanced_map) == 10
+    assert len(advanced_map[0]) == 20
